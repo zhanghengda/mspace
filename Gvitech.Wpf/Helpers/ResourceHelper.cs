8b9257751ee51579493bda37d6b9ba4166fac66d
tony <tony123456>
2020-09-30 11:07:00 +0800

    1

---
+++ Gvitech.Wpf/Helpers/ResourceHelper.cs
@@ -0,0 +1,37 @@
+﻿using Mmc.Windows.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Helpers
+{
+    public class ResourceHelper
+    {
+        public static string FindKey(string keyName)
+        {
+            try
+            {                  
+                if (string.IsNullOrWhiteSpace(keyName)) return "";
+                return (string)Application.Current.FindResource(keyName);
+            }
+            catch (Exception ex)
+            {
+                //todo 打印日志   
+                SystemLog.Log(ex);
+                return null;
+            }
+        }
+
+        public static object FindResourceByKey(string keyName)
+        {
+            var resourceDict = Application.Current.Resources;
+            if (resourceDict == null || resourceDict.Count == 0)
+                return null;
+            var result = Application.Current.TryFindResource(keyName);
+            return result;
+        }
+    }
+}
